5635fd4b16aabde535c3bbc8f6cab4babc3de5ca
Aaron Hennessey <mynamebemrrandom@gmail.com>
2020-12-31 20:00:09 +0000

    oops

--- slate/objects.py
+++ slate/objects.py
@@ -316,7 +316,7 @@ class Track:
         if self.source == 'Youtube':
             return f'https://img.youtube.com/vi/{self.identifier}/mqdefault.jpg'
 
-        if (thumbnail := self._track_info.get('thumbnail', None)) is None:
+        if (thumbnail := self._track_info.get('thumbnail', None)) is not None:
             return thumbnail
 
         return f'https://dummyimage.com/1280x720/000/fff.png&text=+'
